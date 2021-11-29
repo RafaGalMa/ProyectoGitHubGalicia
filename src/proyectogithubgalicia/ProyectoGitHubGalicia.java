039fd876b11f8ecfd21d4af80c966fb674ab532a
RafaGalMa <rafa.galma@gmail.com>
2021-11-29 11:36:12 -0600

    Prueba 1

---
+++ src/proyectogithubgalicia/ProyectoGitHubGalicia.java
@@ -0,0 +1,19 @@
+
+package proyectogithubgalicia;
+
+/**
+ *
+ * @author Rafa
+ */
+public class ProyectoGitHubGalicia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("Prueba Git Hub Esfuerzo 1: " + i);
+        }
+    }
+    
+}
